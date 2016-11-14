cfb61cd6df7e4a9152be84b7648001f87f0692fc
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-11-14 17:41:51 +0200

    added date attributes

--- date.py
+++ date.py
@@ -1,4 +1,7 @@
 class Date:
     """docstring for Terminal"""
     def __init__(self, date, month, year, hour):
-        pass
+        self.date = date
+        self.month = month
+        self.year = year
+        self.hour = hour
