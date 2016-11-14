dfab15b782e1c77b67cf90d03d6f0f7029022093
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-11-14 18:42:29 +0200

    added property name to airline

--- airline.py
+++ airline.py
@@ -1,7 +1,8 @@
 class Airline:
     """docstring for Airline"""
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.all_flights = []
         self.all_passangers = []
         self.all_reservations = []
