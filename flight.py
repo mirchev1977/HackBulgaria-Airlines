178e4aeba7b94e014661e51ddc4acf8b03a49b53
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2016-11-14 17:42:25 +0200

    added flight attributes

--- flight.py
+++ flight.py
@@ -5,4 +5,12 @@ class Flight():
             self, start_time, end_time, passengers,
             max_passengers, from_dest,
             to_dest, terminal, declined):
-        pass
+
+        self.start_time = start_time
+        self.end_time = end_time
+        self.passengers = passengers
+        self.max_passengers = max_passengers
+        self.from_dest = from_dest
+        self.to_dest = to_dest
+        self.terminal = terminal
+        self.declined = declined
